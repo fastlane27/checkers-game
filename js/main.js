fb367cea95b7859f2f14b24603ecb763a9bb7fb8
David Lane <fastlane27@gmail.com>
2020-02-10 17:20:01 -0600

    Select and move pieces

--- js/main.js
+++ js/main.js
@@ -5,8 +5,14 @@ class GamePiece {
         this.piece = document.createElement('span');
     }
     positionPiece(position) {
-        if (this.player === 1) this.piece.style.backgroundColor = 'black';
-        if (this.player === -1) this.piece.style.backgroundColor = 'red';
+        if (this.player === 1) {
+            this.piece.style.backgroundColor = 'black';
+            this.piece.dataset.idx = this.player;
+        }
+        if (this.player === -1) {
+            this.piece.style.backgroundColor = 'red';
+            this.piece.dataset.idx = this.player;
+        }
         position.appendChild(this.piece);
     }
 }
@@ -22,10 +28,11 @@ const squareEls = document.querySelectorAll('.playable');
 
 /*----- event listeners -----*/
 document.getElementById('board').addEventListener('click', selectPiece);
+document.getElementById('board').addEventListener('click', movePiece);
+document.getElementById('reset').addEventListener('click', init);
 
 
 /*----- functions -----*/
-
 init();
 
 function init() {
@@ -39,18 +46,18 @@ function init() {
         [1, 1, 1, 1],
         [1, 1, 1, 1],
     ];
-
     render();
 }
 
 function render() {
     squareEls.forEach(function(square) {
+        if (square.hasChildNodes()) square.firstChild.remove();
         if (board[square.dataset.x][square.dataset.y] === 1) {
-            let blackPiece = new GamePiece(1);
+            let blackPiece = new GamePiece(board[square.dataset.x][square.dataset.y]);
             blackPiece.positionPiece(square);
         } 
         if (board[square.dataset.x][square.dataset.y] === -1) {
-            let redPiece = new GamePiece(-1);
+            let redPiece = new GamePiece(board[square.dataset.x][square.dataset.y]);
             redPiece.positionPiece(square);
         }
     });
@@ -58,5 +65,22 @@ function render() {
 
 function selectPiece(evt) {
     if (evt.target.tagName !== 'SPAN') return;
-    console.log('game piece');
+    squareEls.forEach(function(square) {
+        if (square.firstChild) square.firstChild.classList.remove('selected');
+    });
+    evt.target.classList.add('selected');
+}
+
+function movePiece(evt) {
+    if (evt.target.className !== 'playable') return;
+    if (board[evt.target.dataset.x][evt.target.dataset.y] !== 0) return;
+    squareEls.forEach(function(square) {
+        if (square.firstChild) {
+            if (square.firstChild.className === 'selected') {
+                board[evt.target.dataset.x][evt.target.dataset.y] = parseInt(square.firstChild.dataset.idx);
+                board[square.dataset.x][square.dataset.y] = 0;
+                render();
+            }
+        } 
+    });
 }
